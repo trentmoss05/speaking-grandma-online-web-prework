6e92b8bfc9f48192565cce431ad1863fff699f78
Trent Moss <trentmoss05@gmail.com>
2019-01-01 17:22:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase.upcase
+  if phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
